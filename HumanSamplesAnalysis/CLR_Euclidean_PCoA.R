532d7f25f3d3923ada2618238cba8320fe127792
Vishnu Prasoodanan P K <125027228+vishnuprasoodanan@users.noreply.github.com>
2024-03-15 10:16:41 +0100

    Update CLR_Euclidean_PCoA.R

--- HumanSamplesAnalysis/CLR_Euclidean_PCoA.R
+++ HumanSamplesAnalysis/CLR_Euclidean_PCoA.R
@@ -1,6 +1,5 @@
 #This script necessitates three input files. The 'Human_Genus_count_table.txt' file comprises genus (or ASV) IDs listed in rows and the total count of assigned genera/ASVs in each sample denoted in columns. 'Taxonomy.tsv' includes the genus/ASV IDs in the first column and their corresponding taxonomy in the second column. Lastly, 'Humans_Metadata.txt' contains sample names in the first column and their respective metadata (e.g., individual ID, HD or HF, etc.) listed in the remaining columns.
 
-
 # create PCoA using clr-transformed data
 counts  <- read.csv("Human_Genus_count_table.txt", sep = "\t", row.names = 1)  # Abundance table (e.g. ASV data; to assay data)
 tax     <- as.matrix(read.csv("taxonomy.tsv", sep = "\t", row.names = 1))     # Taxonomy table (to rowData)
@@ -26,14 +25,14 @@ Euclidean_distances <- as.matrix(euclidean_dist)
 Euclidean_dist_column <- melt(Euclidean_distances)
 write.table (Euclidean_dist_column, file = "Euclidean_distances.txt", sep = "\t")
 
-Uni_pcoa <- pcoa(Euclidean_distances)
-Uni_pcoa$values[1:2,]
-mds.var.per = round(Uni_pcoa$values$Eigenvalues/sum(Uni_pcoa$values$Eigenvalues)*100, 1)
+eucl_pcoa <- pcoa(Euclidean_distances)
+eucl_pcoa$values[1:2,]
+mds.var.per = round(eucl_pcoa$values$Eigenvalues/sum(eucl_pcoa$values$Eigenvalues)*100, 1)
 pc <- c(1,2)
-pcoa_df <- Uni_pcoa$vectors[,1:2]
+pcoa_df <- eucl_pcoa$vectors[,1:2]
 jpeg("PCoA.jpg", height = 10, width = 10, units = 'in', res = 600)
-plot(Uni_pcoa$vectors[,1:2], bg= c("skyblue","palegreen", "orangered")[as.factor(as.data.frame(samples)$Status)], pch=21, cex=2, xlab=paste0("PCoA", pc[1], " (", mds.var.per[1], "%)"), ylab=paste0("PCoA", pc[2], " (", mds.var.per[2], "%)"))
-ordiellipse(Uni_pcoa$vectors[,1:2], as.data.frame(samples)$Status, kind="sd", lwd=1, lty=3, draw = "polygon", alpha = 50, col = c("skyblue","palegreen", "orangered"))
+plot(eucl_pcoa$vectors[,1:2], bg= c("skyblue","palegreen", "orangered")[as.factor(as.data.frame(samples)$Status)], pch=21, cex=2, xlab=paste0("PCoA", pc[1], " (", mds.var.per[1], "%)"), ylab=paste0("PCoA", pc[2], " (", mds.var.per[2], "%)"))
+ordiellipse(eucl_pcoa$vectors[,1:2], as.data.frame(samples)$Status, kind="sd", lwd=1, lty=3, draw = "polygon", alpha = 50, col = c("skyblue","palegreen", "orangered"))
 #legend("bottomleft", legend = c("HD", "HF"), col = c("skyblue","palegreen", "orangered"), cex=0.7, title = "", border = "white", fill = NULL, bg="white", bty = "n")
 abline(h=0, v=0, col = "gray60")
 
